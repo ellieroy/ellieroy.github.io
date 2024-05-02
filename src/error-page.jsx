f6a50b20d13f1fb22b4802494ec6c6aa07e4dce3
Ellie <ellie.roy@hotmail.com>
2024-05-02 09:56:12 +0200

    Update background and error page

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -5,12 +5,25 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+    <div
+      id="error-page"
+      style={{
+        height: "100vh",
+        alignContent: "center",
+        display: "grid",
+        justifyContent: "center",
+      }}
+    >
+      <h1 class="xl text-center">Oops!</h1>
+      <p class="text-center">Sorry, an unexpected error has occurred.</p>
+      <p class="text-center">
         <i>{error.statusText || error.message}</i>
       </p>
+      <img
+        src={"src/assets/undraw_fixing_bugs_w7gi.svg"}
+        alt="error"
+        style={{ width: "450px", height: "400px" }}
+      />
     </div>
   );
 }
